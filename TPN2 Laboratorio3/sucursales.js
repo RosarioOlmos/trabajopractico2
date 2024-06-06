8315941faa7f45c2dd8ebdafd7d7582853fad94c
RosarioOlmos <rosarito2603@gmail.com>
2024-06-06 20:38:22 -0300

    n

---
+++ TPN2 Laboratorio3/sucursales.js
@@ -0,0 +1,93 @@
+[
+    {
+        "nombre": "Sucursal Central",
+        "direccion": "Calle Principal 123",
+        "provincia": "Buenos Aires",
+        "telefonos": [
+            {
+                "numero": "123-456-7890",
+                "tipo": "oficina"
+            },
+            {
+                "numero": "123-456-7891",
+                "tipo": "ventas"
+            }
+        ]
+    },
+    {
+        "nombre": "Sucursal Norte",
+        "direccion": "Avenida Norte 456",
+        "provincia": "Córdoba",
+        "telefonos": [
+            {
+                "numero": "234-567-8901",
+                "tipo": "oficina"
+            },
+            {
+                "numero": "234-567-8902",
+                "tipo": "soporte"
+            }
+        ]
+    },
+    {
+        "nombre": "Sucursal Sur",
+        "direccion": "Calle Sur 789",
+        "provincia": "Santa Fe",
+        "telefonos": [
+            {
+                "numero": "345-678-9012",
+                "tipo": "oficina"
+            },
+            {
+                "numero": "345-678-9013",
+                "tipo": "ventas"
+            }
+        ]
+    },
+    {
+        "nombre": "Sucursal Este",
+        "direccion": "Avenida Este 101",
+        "provincia": "Mendoza",
+        "telefonos": [
+            {
+                "numero": "456-789-0123",
+                "tipo": "oficina"
+            },
+            {
+                "numero": "456-789-0124",
+                "tipo": "soporte"
+            }
+        ]
+    },
+    {
+        "nombre": "Sucursal Oeste",
+        "direccion": "Calle Oeste 202",
+        "provincia": "Tucumán",
+        "telefonos": [
+            {
+                "numero": "567-890-1234",
+                "tipo": "oficina"
+            },
+            {
+                "numero": "567-890-1235",
+                "tipo": "ventas"
+            }
+        ]
+    },
+    {
+        "nombre": "Sucursal Noreste",
+        "direccion": "Avenida Noreste 303",
+        "provincia": "Misiones",
+        "telefonos": [
+            {
+                "numero": "678-901-2345",
+                "tipo": "oficina"
+            },
+            {
+                "numero": "678-901-2346",
+                "tipo": "soporte"
+            }
+        ]
+    }
+]
+
